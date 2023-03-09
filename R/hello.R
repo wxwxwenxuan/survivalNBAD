507544ae4239ad180a670a59423bfb34b93fee4c
Wenxuan <1019159757@qq.com>
2023-03-08 21:09:17 -0500

    Initial commit

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,9 @@
-usethis::use_git_config(user.name="wxwxwenxuan", user.email="1019159757@qq.com")
+usethis::use_git_config(user.name="Wenxuan", user.email="1019159757@qq.com")
 usethis::create_github_token()
 usethis::edit_git_config()
 library(usethis)
-use_git()
+use_github()
 
-
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+gitcreds::gitcreds_set()
+3
