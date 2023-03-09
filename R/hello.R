54fe3fa9e768675f848ee0488da35507b893cec5
Wenxuan <1019159757@qq.com>
2023-03-08 21:23:12 -0500

    "gg"

--- R/hello.R
+++ R/hello.R
@@ -1,9 +1,2 @@
-usethis::use_git_config(user.name="Wenxuan", user.email="1019159757@qq.com")
-usethis::create_github_token()
-usethis::edit_git_config()
-library(usethis)
-use_github()
-
-usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
-gitcreds::gitcreds_set()
-3
+usethis::edit_r_environ()
+vcb
